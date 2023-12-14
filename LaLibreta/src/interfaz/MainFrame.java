1dd8266070695f40ff29f0b680ee85a2fc163ec4
Gurjant Singh <96392014+landlord245@users.noreply.github.com>
2023-12-14 18:15:14 +0100

    sd

--- LaLibreta/src/interfaz/MainFrame.java
+++ LaLibreta/src/interfaz/MainFrame.java
@@ -1,59 +1,18 @@
 package interfaz;
 
-import interfaz.panels.Hub;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
-    private Hub hub;
-    private Color colorPrimario;
-    private Color colorSecundario;
-    private Color colorOscuro;
-    public MainFrame() {
-        colorPrimario = new Color(169, 218, 255);
-        colorSecundario = new Color(16, 151, 255);
-        colorOscuro = new Color(0, 60, 106);
+    private Dimension dimension;
+
+    public MainFrame(Dimension dimension){
+        this.dimension = dimension;
 
         this.setTitle("La Libreta");
-        this.setSize(500, 500);
+        this.setSize(this.dimension);
         this.setLocationRelativeTo(this);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        hub = new Hub(this.getSize(), colorPrimario, colorSecundario, colorOscuro);
-        this.add(hub);
         this.setVisible(true);
-    }
-
-    public Hub getHub() {
-        return hub;
-    }
-
-    public void setHub(Hub hub) {
-        this.hub = hub;
-    }
-
-    public Color getColorPrimario() {
-        return colorPrimario;
-    }
-
-    public void setColorPrimario(Color colorPrimario) {
-        this.colorPrimario = colorPrimario;
-    }
-
-    public Color getColorSecundario() {
-        return colorSecundario;
-    }
-
-    public void setColorSecundario(Color colorSecundario) {
-        this.colorSecundario = colorSecundario;
-    }
-
-    public Color getColorOscuro() {
-        return colorOscuro;
-    }
-
-    public void setColorOscuro(Color colorOscuro) {
-        this.colorOscuro = colorOscuro;
     }
 }
